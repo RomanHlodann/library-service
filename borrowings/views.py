68f0d5b474f289c962750b554f639cb024506218
RomanHlodann <roman.hlodann@gmail.com>
2024-05-20 18:29:45 +0300

    replace if condition in views for serializers by raise_exception=True flag

--- borrowings/views.py
+++ borrowings/views.py
@@ -103,11 +103,6 @@ class BorrowingViewSet(
             book.save()
 
             serializer = self.get_serializer(borrowing, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
