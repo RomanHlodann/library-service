abc48fdb18f028d9a9bc04b976d82149f2095b10
RomanHlodann <roman.hlodann@gmail.com>
2024-05-20 18:32:24 +0300

    add related_name to foreign keys & migrations

--- borrowings/models.py
+++ borrowings/models.py
@@ -12,10 +12,14 @@ class Borrowing(models.Model):
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(blank=True, null=True)
     book = models.ForeignKey(
-        Book, on_delete=models.CASCADE
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
     )
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
     )
 
     @staticmethod
